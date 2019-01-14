926af2727c6cd08799a722f8eb242227698187f2
DEV Lukas <Lukas.Baltrenas@ev.lt>
2019-01-14 21:28:11 +0200

    lower

--- WebApplication1/IService1.cs
+++ WebApplication1/IService1.cs
@@ -16,8 +16,22 @@ namespace WebApplication1
         [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "AddSchedule/{Municipality}/{TaxType}/{Date}")]
         string AddSchedule(string Municipality, string TaxType,string Date);
 
+        //[OperationContract]
+        //[WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "PosVersion/{StoreNo}/{PosNo}/{IP}/{VersionNo}")]
+        //string LogPosVersion(string StoreNo, string PosNo, string IP, string VersionNo);
+
+        //[OperationContract]
+        //[WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "AdmVersion/{StoreNo}/{PosNo}/{IP}/{VersionNo}")]
+        //string LogAdmVersion(string StoreNo, string PosNo, string IP, string VersionNo);
+
+        //[OperationContract]
+        //[WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "ErecipeWarning/{StoreNo}/{PosNo}/{IP}/{Count0}/{Count1}")]
+        //string LogErecipeStatus(string StoreNo, string PosNo, string IP, string Count0, string Count1);
+
         [OperationContract]
-        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "GetTax/{Municipality}/{Date}")]
-        string GetTax(string Municipality, string Date);                
+        
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "msg/{StoreNo}/{PosNo}")]
+        string GetMessage(string StoreNo, string PosNo);
+
     }
 }
