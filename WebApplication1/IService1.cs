e754d9cb962cbfb1ff0a1712b5100a5c285f5aac
DEV Lukas <Lukas.Baltrenas@ev.lt>
2019-01-14 19:21:31 +0200

    SecondOne

---
+++ WebApplication1/IService1.cs
@@ -0,0 +1,37 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web.UI;
+
+namespace WebApplication1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "log/{StoreNo}/{PosNo}/{IP}/{ErrType}")]
+        string LogEvent(string StoreNo, string PosNo, string IP, string ErrType);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "AddSchedule/{Municipality}/{TaxType}/{Date}")]
+        string AddSchedule(string Municipality, string TaxType,string Date);
+
+        //[OperationContract]
+        //[WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "PosVersion/{StoreNo}/{PosNo}/{IP}/{VersionNo}")]
+        //string LogPosVersion(string StoreNo, string PosNo, string IP, string VersionNo);
+
+        //[OperationContract]
+        //[WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "AdmVersion/{StoreNo}/{PosNo}/{IP}/{VersionNo}")]
+        //string LogAdmVersion(string StoreNo, string PosNo, string IP, string VersionNo);
+
+        //[OperationContract]
+        //[WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "ErecipeWarning/{StoreNo}/{PosNo}/{IP}/{Count0}/{Count1}")]
+        //string LogErecipeStatus(string StoreNo, string PosNo, string IP, string Count0, string Count1);
+
+        [OperationContract]
+        
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "msg/{StoreNo}/{PosNo}")]
+        string GetMessage(string StoreNo, string PosNo);
+
+    }
+}
---
+++ WebApplication1/Service1.svc.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        public string GetMessage(string StoreNo, string PosNo)
+        {
+            return "Hello";
+        }
+
+        public string LogEvent(string StoreNo, string PosNo, string IP, string ErrType)
+        {
+            int store, pos;
+
+            string body = "";
+
+            if (!(int.TryParse(StoreNo, out store) && (int.TryParse(PosNo, out pos))))
+                return "Err param";
+
+            if (OperationContext.Current.RequestContext.RequestMessage.IsEmpty)
+                return "Err body";
+            body = Encoding.UTF8.GetString(OperationContext.Current.RequestContext.RequestMessage.GetBody<byte[]>());
+
+            if (body.Length < 4)
+                return "Err body";
+            string[] MunicipalityArray;
+
+            MunicipalityArray = body.Split('\n');
+            return MunicipalityArray.Count().ToString();
+    
+
+        }
+
+        public string AddSchedule(string Municipality, string TaxType, string Date)
+        {
+            string MunID,TaxID;
+          
+            try
+            {
+                SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Connectas"].ConnectionString);
+                conn.Open();
+                {
+                   
+                    using (SqlCommand cmd = new SqlCommand("Select ID from Municipality where [Municipality] = '"+ Municipality + "'", conn))
+                    {
+                                               
+                        cmd.ExecuteNonQuery();
+                        MunID = cmd.ExecuteScalar().ToString();
+
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand("Select ID from TaxesConfig where [TaxTypeString] = '" + TaxType + "'", conn))
+                    {
+
+                        cmd.ExecuteNonQuery();
+                        TaxID = cmd.ExecuteScalar().ToString();
+
+                    }
+
+                    //INSERTAS EINA CIA
+
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO [Taxes] values ("+ MunID +"," + TaxID + ",'" + Date + "','2018-01-14')", conn))
+                    {
+
+                        cmd.ExecuteNonQuery();
+                       
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                return ex.ToString();
+                //Log exception
+                //Display Error message
+            }
+            return "OK";
+        }
+
+
+    }
+}
